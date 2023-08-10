da9ad2f05fc808a8edf70264cc4bf5ef70148df6
KermieTheFrog <96618381+KermieTheFrog@users.noreply.github.com>
2023-08-09 19:54:03 -0500

    Create infer.py

---
+++ infer.py
@@ -0,0 +1,61 @@
+import gradio as gr
+from gradio import components
+from pathlib import Path
+import json
+import shutil
+import os
+
+if not 'initialised' in globals():
+    raise NameError('Please run the first cell first!! #scrollTo=H_cTbwhVq4K6')
+
+with open('model_data.json', 'r') as f:
+  model_data = json.load(f)
+
+tracks_path = '/content/drive/MyDrive/MDX_Colab/tracks/'
+separated_path = 'separated/'
+
+def process_data(input_path, Denoise, Normalize, Chunks, Shifts):
+    
+
+    track = Path(input_path.name)
+    track = track.stem + track.suffix
+    track = f"{tracks_path}{track}"
+    shutil.move(input_path.name, "/content/drive/MyDrive/MDX_Colab/tracks")
+    
+    denoise = Denoise
+    normalise = Normalize
+
+    amplitude_compensation = model_data["(de)Reverb HQ By FoxJoy"]["compensate"]
+    dim_f = model_data["(de)Reverb HQ By FoxJoy"]["mdx_dim_f_set"]
+    dim_t = model_data["(de)Reverb HQ By FoxJoy"]["mdx_dim_t_set"]
+    n_fft = model_data["(de)Reverb HQ By FoxJoy"]["mdx_n_fft_scale_set"]
+
+    mixing_algorithm = 'min_mag' 
+    chunks = Chunks
+    shifts = Shifts
+
+    ##validate values
+    normalise = '--normalise' if normalise else ''
+    denoise = '--denoise' if denoise else ''
+    margin = 44100
+
+    console(f"python main.py --n_fft {n_fft} --dim_f {dim_f} --dim_t {dim_t} --margin {margin} -i \"{track}\" --mixing {mixing_algorithm} --onnx \"{'onnx/(de)Reverb HQ By FoxJoy'}\" --model off  --shifts {round(shifts)} --stems v --invert v --chunks {chunks} --compensate {amplitude_compensation} {normalise} {denoise}")
+    os.remove(track)
+    return f'De reverb complete! Please check "/content/drive/MyDrive/MDX_Colab/separated" for your results.'
+
+input_path = components.File()
+
+Denoise = components.Checkbox(label="Denoise", value=True)
+Normalize = components.Checkbox(label="Normalize")
+Chunks = components.Slider(label="Chunks", minimum=1, maximum=55, value=55)
+
+Shifts = components.Slider(label="Shifts", minimum=0, maximum=10, value=10)
+
+interface = gr.Interface(
+    fn=process_data,
+    inputs=[input_path, Denoise, Normalize, Chunks, Shifts],
+    outputs="text",
+    allow_flagging='never'
+)
+
+interface.launch(share = True, debug = True)
