8fb0b4a2277077d87ce6e90ccfc86af050287f5a
KermieTheFrog <96618381+KermieTheFrog@users.noreply.github.com>
2023-08-09 20:17:22 -0500

    Fixed infer.py

--- infer.py
+++ infer.py
@@ -1,9 +1,11 @@
+# @title Inference
 import gradio as gr
 from gradio import components
 from pathlib import Path
 import json
-import shutil
 import os
+import shutil
+
 
 with open('model_data.json', 'r') as f:
   model_data = json.load(f)
@@ -12,33 +14,29 @@ tracks_path = '/content/drive/MyDrive/MDX_Colab/tracks/'
 separated_path = 'separated/'
 
 def process_data(input_path, Denoise, Normalize, Chunks, Shifts):
-    
+  track = Path(input_path.name)
+  track = track.stem + track.suffix
+  shutil.move(input_path.name, tracks_path)
+  track = tracks_path+track
 
-    track = Path(input_path.name)
-    track = track.stem + track.suffix
-    track = f"{tracks_path}{track}"
-    shutil.move(input_path.name, "/content/drive/MyDrive/MDX_Colab/tracks")
-    
-    denoise = Denoise
-    normalise = Normalize
+  denoise = Denoise
+  normalise = Normalize
 
-    amplitude_compensation = model_data["(de)Reverb HQ By FoxJoy"]["compensate"]
-    dim_f = model_data["(de)Reverb HQ By FoxJoy"]["mdx_dim_f_set"]
-    dim_t = model_data["(de)Reverb HQ By FoxJoy"]["mdx_dim_t_set"]
-    n_fft = model_data["(de)Reverb HQ By FoxJoy"]["mdx_n_fft_scale_set"]
+  amplitude_compensation = model_data["(de)Reverb HQ By FoxJoy"]["compensate"]
+  dim_f = model_data["(de)Reverb HQ By FoxJoy"]["mdx_dim_f_set"]
+  dim_t = model_data["(de)Reverb HQ By FoxJoy"]["mdx_dim_t_set"]
+  n_fft = model_data["(de)Reverb HQ By FoxJoy"]["mdx_n_fft_scale_set"]
 
-    mixing_algorithm = 'min_mag' 
-    chunks = Chunks
-    shifts = Shifts
+  mixing_algorithm = 'min_mag' 
+  chunks = Chunks
+  shifts = Shifts
 
-    ##validate values
-    normalise = '--normalise' if normalise else ''
-    denoise = '--denoise' if denoise else ''
-    margin = 44100
-
-    console(f"python main.py --n_fft {n_fft} --dim_f {dim_f} --dim_t {dim_t} --margin {margin} -i \"{track}\" --mixing {mixing_algorithm} --onnx \"{'onnx/(de)Reverb HQ By FoxJoy'}\" --model off  --shifts {round(shifts)} --stems v --invert v --chunks {chunks} --compensate {amplitude_compensation} {normalise} {denoise}")
-    os.remove(track)
-    return f'De reverb complete! Please check "/content/drive/MyDrive/MDX_Colab/separated" for your results.'
+  ##validate values
+  normalise = '--normalise' if normalise else ''
+  denoise = '--denoise' if denoise else ''
+  margin = 44100
+  os.system(f"python main.py --n_fft {n_fft} --dim_f {dim_f} --dim_t {dim_t} --margin {margin} -i \"{track}\" --mixing {mixing_algorithm} --onnx \"{'onnx/(de)Reverb HQ By FoxJoy'}\" --model off  --shifts {round(shifts)} --stems v --invert v --chunks {chunks} --compensate {amplitude_compensation} {normalise} {denoise}")
+  return "De reverb complete!"
 
 input_path = components.File()
 
@@ -55,4 +53,6 @@ interface = gr.Interface(
     allow_flagging='never'
 )
 
-interface.launch(share = True, debug = True)
+
+
+interface.launch(share = True)
